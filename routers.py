71f4dc0e22aeecbacaac0e4a678503c06f7af153
AfonsoDolmen <quadradoplay@outlook.com>
2025-02-09 13:27:49 -0300

    Criado nova rota com body parameter.

--- routers.py
+++ routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Path, Query
 from converter import async_converter
+from schemas import ConverterInput, ConverterOutput
 
 
 router = APIRouter()
@@ -25,3 +26,28 @@ async def converter(
         result.append(response)
     
     return result
+
+
+@router.get('/converter/v2/{from_currency}', response_model=ConverterOutput)
+async def converter(
+    body: ConverterInput,
+    from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$')
+):
+    to_currencies = body.to_currencies
+    price = body.price
+
+    result = []
+
+    for currency in to_currencies:
+        response = await async_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+
+        result.append(response)
+    
+    return ConverterOutput(
+        message='success',
+        data=result
+    )
